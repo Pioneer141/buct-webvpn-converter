4945939b8568ab20a7e39855ec41c0feb233c528
Pioneer-117 <w982125792@gmail.com>
2023-11-13 21:02:05 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,14 @@
 const Footer = () => (
   <footer className="bg-zinc-200/30 dark:bg-zinc-500/10 p-4 text-center dark:text-gray-400 text-sm">
-    Created with love at BIT
+    Created with love at BUCT
     <br />
     <a
-      href="https://spencerwoo.com"
+      href="https://engineerlion.tech"
       target="_blank"
       rel="noopener noreferrer"
       className="underline opacity-80 hover:opacity-100 transition-all duration-50"
     >
-      Spencer Woo
+      Pioneer117
     </a>{' '}
     @ 2020 - {new Date().getFullYear()}
   </footer>
